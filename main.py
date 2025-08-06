5e64adecf5b4ff9e2a0c8492dfb59ca5c361d305
VU-SON-HAI01 <Vshai48kd1@gmail.com>
2025-08-07 01:50:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -135,11 +135,8 @@ def laykhoatu_magv(df_khoa, magv):
     return "Không tìm thấy khoa"
 
 # --- GIAO DIỆN ỨNG DỤNG ---
-col1, col2, col3 = st.columns([1, 1, 1])
-with col2:
-    # Giả sử bạn có thư mục 'image' cùng cấp với file app.py
-    st.image("image/Logo_caodangdaklak_top.png",width = 150)
 
+st.image("image/Logo_caodangdaklak_top.png",width = 150)
 st.markdown("<h1 style='text-align: center; color: green;'>KÊ GIỜ NĂM HỌC 2025</h1>", unsafe_allow_html=True)
 
 # Khởi tạo OAuth2Component
