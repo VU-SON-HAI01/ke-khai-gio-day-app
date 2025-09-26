f1759140289e2b06d184139e1c4cf488a6e395c3
Vũ Hải <vshai48kd1@gmail.com>
2025-09-26 14:58:57 +0700

    Enhance load_all_mon_data: add debug output for mon_hoc_data initialization

--- quydoi_gioday.py
+++ quydoi_gioday.py
@@ -585,9 +585,10 @@ def save_data_to_sheet(worksheet_name, data_to_save):
 
 def load_all_mon_data():
     """Tải tất cả dữ liệu môn học đã lưu của GV từ Google Sheet."""
-    st.write(st.session_state.mon_hoc_data)
+    
     st.session_state.mon_hoc_data = []
     st.session_state.results_data = []
+    st.write(st.session_state.mon_hoc_data)
     all_worksheets = [ws.title for ws in spreadsheet.worksheets()]
     
     # Chỉ dùng 1 sheet cho input và 1 sheet cho output
