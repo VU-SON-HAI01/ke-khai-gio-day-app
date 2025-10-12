a72509289439a02a0daa16ebec3e1369f0b0485d
Vũ Hải <vshai48kd1@gmail.com>
2025-10-12 18:05:50 +0700

    Remove debug output for HK2 data in export_giangday_to_excel function

--- tonghop_kegio.py
+++ tonghop_kegio.py
@@ -85,8 +85,6 @@ def export_giangday_to_excel(spreadsheet=None, df_mon=None, df_hk1=None, templat
 
         if ws_hk2 is not None and 'Ke_gio_HK2_Cả_năm' in wb.sheetnames:
             df_hk2 = pd.DataFrame(ws_hk2.get_all_records())
-            st.write("Dữ liệu từ output_giangday(HK2):")
-            st.write(df_hk2)
             sheet_hk2 = wb['Ke_gio_HK2_Cả_năm']
             for i, row in df_hk2.iterrows():
                 excel_row = int(start_row + i)
