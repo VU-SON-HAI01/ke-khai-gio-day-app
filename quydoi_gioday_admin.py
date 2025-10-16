1f4ccd0af6f2c64d8f3b1e54bb9d360191968d66
Vũ Hải <vshai48kd1@gmail.com>
2025-10-16 16:59:40 +0700

    Refactor API call delay handling and improve status updates during data download

--- quydoi_gioday_admin.py
+++ quydoi_gioday_admin.py
@@ -1028,17 +1028,8 @@ try:
         df_tonghop = []
         for sheet_info in sheet_names:
             import time
-                # Thêm delay giữa các lần gọi API để tránh vượt quota
-            with st.status("Downloading data...", expanded=True) as status:
-                st.write("Searching for data...")
-                time.sleep(2)
-                st.write("Found URL.")
-                time.sleep(1)
-                st.write("Downloading data...")
-                time.sleep(1)
-                status.update(
-                    label=f"{sheet_info} - Download complete!", state="complete", expanded=False
-                )
+            # Thêm delay giữa các lần gọi API để tránh vượt quota
+            time.sleep(1)
             ma_gv = str(sheet_info).split(' ')[0]
             sheet_id = sheet_info.split('(')[-1].replace(')', '')
             ten_gv_from_sheet = None
