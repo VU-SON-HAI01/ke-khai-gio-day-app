8c791e2f39e05b746e2b80ac5190b8f3726f13c8
Vũ Hải <vshai48kd1@gmail.com>
2026-01-13 11:28:41 +0700

    Remove unnecessary line break in address display logic for improved clarity

--- quanlyhssv.py
+++ quanlyhssv.py
@@ -322,7 +322,6 @@ with col2:
             diachi_chitiet_cu = f"{duong_pho}, {thon_xom_loai} {thon_xom}" if thon_xom_loai != "Không" else f"{duong_pho}"
             st.write(f"Địa chỉ cũ: :blue[{diachi_chitiet_cu}, {ward_idx}, {district_idx}, {province_idx}]")
         st.session_state["diachi_chitiet_cu"] = diachi_chitiet_cu
-        st.markdown("<br>", unsafe_allow_html=True)
         # Nút xác nhận địa chỉ động như API_diachi
         #if st.button("Xác nhận địa chỉ", key="xacnhan_diachi_cu"):
         if province_code and district_code and ward_code:
