c502305e93a94d477cda3536ed9e63f185f91c90
Vũ Hải <vshai48kd1@gmail.com>
2026-01-14 15:21:34 +0700

    Update inline styles for improved readability and consistency

--- quanlyhssv.py
+++ quanlyhssv.py
@@ -592,10 +592,10 @@ with col3:
         n = len(keys)
         col1, col2 = st.columns(2)
         split = n // 2 + (n % 2)
-        style_macdinh = "font-weight:normal;display:inline;line-height:1;padding:0;margin:0"
-        style_xanh = "color:green;font-weight:normal;display:inline;line-height:1;padding:0;margin:0"
-        style_cam = "color:Orange;font-weight:normal;display:inline;line-height:1;padding:0;margin:0"
-        style_do = "color:Red;font-weight:normal;display:inline;line-height:1;padding:0;margin:0"
+        style_macdinh = "font-weight:normal;display:inline;line-height:0.8;font-size:15px;padding:0;margin:0"
+        style_xanh = "color:green;font-weight:normal;display:inline;line-height:0.8;font-size:15px;padding:0;margin:0"
+        style_cam = "color:Orange;font-weight:normal;display:inline;line-height:0.8;font-size:15px;padding:0;margin:0"
+        style_do = "color:Red;font-weight:normal;display:inline;line-height:0.8;font-size:15px;padding:0;margin:0"
         truong_bat_buoc = ["Họ và tên", "Ngày sinh", "CCCD"]
         with col1:
             for k in keys[:split]:
