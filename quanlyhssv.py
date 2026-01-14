674099b434d798d2f5f6c7be0289fd0577623abe
Vũ Hải <vshai48kd1@gmail.com>
2026-01-14 15:28:04 +0700

    Update info message to clarify required and optional data fields

--- quanlyhssv.py
+++ quanlyhssv.py
@@ -615,7 +615,7 @@ with col3:
                 else:
                     style = style_cam if is_empty else style_xanh
                 st.markdown(f"<div style='line-height:1.8;font-size:15px;padding:0;margin:0'><span style='{style}'><b>{k}:</b> </span><span style='{style_macdinh}'>{value}</span></div>", unsafe_allow_html=True)
-        st.info("Nếu thông tin đã chính xác, hãy nhấn 'Lưu tất cả thông tin' để hoàn tất.")
+        st.info("Màu đỏ là dữ liệu bắt buộc phải nhập, màu cam là dữ liệu không bắt buộc. Nếu thông tin đã chính xác, hãy nhấn 'Lưu tất cả thông tin' để hoàn tất.")
 
     if st.button("Xem lại thông tin"):
         show_review_dialog()
