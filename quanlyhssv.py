c6c27d3602543f9222d9265b1b58e7d0868c826a
Vũ Hải <vshai48kd1@gmail.com>
2026-01-21 10:57:51 +0700

    Refactor ho_ten input handling to utilize render_special_char_buttons for improved special character management

--- quanlyhssv.py
+++ quanlyhssv.py
@@ -143,7 +143,8 @@ with col1:
     # Các ký tự đặc biệt của Tên Tây nguyên
     with st.popover("Ký tự đặc biệt",icon="🔣"):
         st.session_state.get("ho_ten", "") = render_special_char_buttons(st.session_state["ho_ten"] if "ho_ten" in st.session_state else "")
-    ho_ten = st.text_input(":green[HỌ VÀ TÊN]", value=st.session_state.get("ho_ten", ""))
+    ho_ten = render_special_char_buttons(st.session_state.get("ho_ten", ""))
+    ho_ten = st.text_input(":green[HỌ VÀ TÊN]", value=ho_ten)
     st.session_state["ho_ten"] = ho_ten
     import datetime
     ngay_sinh = st.date_input(
