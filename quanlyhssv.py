f4e2868b91a9a6b60a408c18216ba22c48594383
Vũ Hải <vshai48kd1@gmail.com>
2026-01-21 10:51:48 +0700

    Update popover icon for special characters to enhance visual clarity

--- quanlyhssv.py
+++ quanlyhssv.py
@@ -168,7 +168,7 @@ with col1:
         unsafe_allow_html=True
     )
     # Các ký tự đặc biệt của Tên Tây nguyên
-    with st.popover("Ký tự đặc biệt",icon="ℹ🔣"):
+    with st.popover("Ký tự đặc biệt",icon="🔣"):
         render_special_char_buttons(st.session_state["ho_ten"] if "ho_ten" in st.session_state else "")
     ho_ten = st.text_input(":green[HỌ VÀ TÊN]", value=st.session_state.get("ho_ten", ""))
     st.session_state["ho_ten"] = ho_ten
