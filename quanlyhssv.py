42334fd2d530eea90cac18fcdbe484dd5889fd3c
Vũ Hải <vshai48kd1@gmail.com>
2026-02-03 16:09:50 +0700

    Reset default value for CCCD issuing authority selection

--- quanlyhssv.py
+++ quanlyhssv.py
@@ -575,7 +575,7 @@ with col1:
         "Cục Cảnh sát ĐKQL cư trú và DLQG về dân cư",
         "Khác",
     ]
-    noi_cap_default = st.session_state.get("noi_cap_cccd", "")
+    noi_cap_default = ""
     noi_cap_cccd = st.selectbox("Nơi cấp CCCD:", options=noi_cap_options, index=noi_cap_options.index(noi_cap_default))
     st.session_state["noi_cap_cccd"] = noi_cap_cccd
     if cccd:
