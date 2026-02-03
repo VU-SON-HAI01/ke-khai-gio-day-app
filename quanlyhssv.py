810886f940a0f108d9829e29d394e8d606235e51
Vũ Hải <vshai48kd1@gmail.com>
2026-02-03 15:06:03 +0700

    Update review button for information display to enhance user interaction

--- quanlyhssv.py
+++ quanlyhssv.py
@@ -1074,9 +1074,9 @@ with col3:
         st.session_state["nv2"] = nv2
         nv3 = st.selectbox(":green[NGUYỆN VỌNG 3]", nganh_options, index=nganh_options.index(st.session_state.get("nv3", nganh_options[0])) if st.session_state.get("nv3", nganh_options[0]) in nganh_options else 0)
         st.session_state["nv3"] = nv3
-    if st.button("Xem lại thông tin"):
-        show_review_dialog()     
-        # Nút lưu tổng cuối trang
+        if st.button("Xem lại thông tin",type="primary",key="btn_review_info",use_container_width=True):
+            show_review_dialog()     
+
 
 # Phần 4: Cấu hình tên file và trang tính QL HSSV
 target_folder_name_hssv = st.secrets["target_folder_name_hssv"] if "target_folder_name_hssv" in st.secrets else "QUAN_LY_HSSV"
