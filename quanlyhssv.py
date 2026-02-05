991771fb36c4d77d778695a64ccbf7be167d6823
Vũ Hải <vshai48kd1@gmail.com>
2026-02-05 14:46:33 +0700

    Fix header icon placement in application form for improved visual consistency

--- quanlyhssv.py
+++ quanlyhssv.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Quản lý HSSV", layout="wide")
 st.markdown(
 # Hiển thị tiêu đề lớn
     """
-    📝 <span style='font-size:24px; font-weight:bold;'>NHẬP HỒ SƠ TUYỂN SINH</span><br>
+     <span style='font-size:24px; font-weight:bold;'>📝NHẬP HỒ SƠ TUYỂN SINH</span><br>
     """,
     unsafe_allow_html=True
 )
