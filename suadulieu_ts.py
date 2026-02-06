47a888d4071dcd2df5e359d8ab7649e8a23e6cfa
Vũ Hải <vshai48kd1@gmail.com>
2026-02-06 09:54:25 +0700

    Add current year display in enrollment year filter dialog

--- suadulieu_ts.py
+++ suadulieu_ts.py
@@ -262,6 +262,7 @@ def update_dialog():
     # Bộ lọc bắt buộc theo Năm tuyển sinh (lọc theo 2 số đầu của Mã HSTS)
     # Lấy danh sách năm từ dữ liệu, mặc định lấy từ 2020 đến năm hiện tại
     current_year = datetime.date.today().year
+    st.write(current_year)
     years = list(range(2020, current_year + 1))
     years_str = [str(y) for y in years]
     nam_tuyensinh = st.selectbox("Chọn năm tuyển sinh:", years_str, index=len(years_str)-1, key="nam_tuyensinh_filter")
